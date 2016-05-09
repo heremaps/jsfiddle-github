683044db96ad96426e6522d45e6491be73090ec0
Jason Fox <jason.fox@here.com>
2016-05-09 16:13:54 +0200

    Add jQuery <script> to header and reference the jQuery website and link to its MIT license.

--- marker-clustering/demo.js
+++ marker-clustering/demo.js
@@ -62,6 +62,12 @@ var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 var ui = H.ui.UI.createDefault(map, defaultLayers);
 
 // Step 5: request data that will be visualized on a map
+//
+//  For convenience we use the jQuery library make a dummy AJAX call to do this.
+//  For more information see: http://api.jquery.com/jQuery.getJSON/
+//
+//  The jQuery library is available under an MIT license  https://jquery.org/license/
+//
 var url= '/gh/get/response.json/heremaps/jsfiddle-github/master/marker-clustering/';
 $.ajax({
     type: 'GET',
